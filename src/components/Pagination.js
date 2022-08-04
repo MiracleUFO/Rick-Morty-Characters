a798c0c67de37d2d0dfcf8a318a3e40260816900
Miracle Ufodiama <miracleufodiama@gmail.com>
2022-08-04 21:51:22 +0100

    fix: right pagination batch now showing even after page has been updated

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { updateCurrentPage } from '../redux/reducers/pages';
+import { updateCurrentPage, updateDisplayedPageNumbers, setCurrentDisplayedBatch } from '../redux/reducers/pages';
 
 import {
     PaginationContainer,
@@ -12,10 +12,8 @@ import {
 const Pagination = () => {
     const 
         dispatch = useDispatch(),
-        { pages, currentPage } = useSelector(state => state.pages),
-        [allPages, setAllPages] = useState([]),
-        [currentDisplayedPages, setCurrentDisplayedPages] = useState([]),
-        [currentDisplayedBatch, setCurrentDisplayedBatch] = useState(1)
+        { pages, currentPage, displayedPageNumbers, currentDisplayedBatch } = useSelector(state => state.pages),
+        [allPages, setAllPages] = useState([])
     ;
 
     // Creates array of pages [1, 2, 3, ...pages]
@@ -24,8 +22,8 @@ const Pagination = () => {
     // Shows needed batch of 5 page numbers when arrowbuttons are clicked
     useEffect(() => {
         const start = currentDisplayedBatch === 1 ? 0 : 5 * (currentDisplayedBatch - 1);
-        setCurrentDisplayedPages(allPages.slice(start, start + 5));
-    }, [allPages, currentDisplayedBatch]);
+        dispatch(updateDisplayedPageNumbers(allPages.slice(start, start + 5)));
+    }, [allPages, currentDisplayedBatch, dispatch]);
 
     // Scrolls to top when new page is loaded
     useEffect(() => {
@@ -42,13 +40,13 @@ const Pagination = () => {
                     <PaginationContainer id='pagination-container'>
                         <ArrowButton 
                             disabled={currentDisplayedBatch === 1}
-                            onClick={() => setCurrentDisplayedBatch(currentDisplayedBatch - 1)}
+                            onClick={() => dispatch(setCurrentDisplayedBatch(currentDisplayedBatch - 1))}
                         >
                             &larr;
                         </ArrowButton>
 
                         <PageNumbersContainer>
-                            {currentDisplayedPages.map(x =>
+                            {displayedPageNumbers.map(x =>
                                 <PageButton 
                                     key={x} 
                                     currentPage={currentPage - 1 === x} 
@@ -61,7 +59,7 @@ const Pagination = () => {
 
                         <ArrowButton 
                             disabled={currentDisplayedBatch === Math.ceil(pages / 5)}
-                            onClick={() => setCurrentDisplayedBatch(currentDisplayedBatch + 1)}
+                            onClick={() => dispatch(setCurrentDisplayedBatch(currentDisplayedBatch + 1))}
                         >
                             &rarr;
                         </ArrowButton>
